cf50f59fe79a386e05c852837623d54916c1b090
Denise <deniselewis1120@gmail.com>
2023-12-05 09:27:07 -0800

    Fetch and display recipe cards

--- src/App.js
+++ src/App.js
@@ -1,12 +1,37 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
+import RecipeExcerpt from "./components/RecipeExcerpt";
 import "./App.css";
 
 function App() {
+  const [recipes, setRecipes] = useState([]);
+
+  // Fetch all recipes
+  const fetchAllRecipes = async () => {
+    try {
+      const response = await fetch("/api/recipes");
+      if (response.ok) {
+        const data = await response.json();
+        setRecipes(data);
+      } else {
+        console.log("Could not fetch recipes");
+      }
+    } catch (error) {
+      console.error("An error occurred during the request: ", error);
+      console.log("An unexpected error occurred. Please try again later.");
+    }
+  };
+
+  useEffect(() => {
+    fetchAllRecipes();
+  }, []);
+
   return (
     <div className='recipe-app'>
       <Header />
-      <p>Your recipes here! </p>
+      <div className="recipe-list">
+        {recipes.map((recipe, index) => <RecipeExcerpt recipe={recipe} key={index} />)}
+      </div>
     </div>
   );
 }
